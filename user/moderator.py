910470eb111df568f1fb57dadbb09df5a8350109
bencyn <bensonnjung39@gmail.com>
2019-01-21 16:14:03 +0300

    [Start #163362242] initialize comment class

--- user/moderator.py
+++ user/moderator.py
@@ -1,11 +1,13 @@
 from user.user import User, UserCollection
+from comments.comment import Comment
 class Moderator(User):
     
     def __init__(self):
         super.__init__()
-        
-    def edit_comment(self):
-        pass
-    
+        self.comment = Comment()
+    def edit_comment(self,comment_id,message):
+        """ allow moderator to edit comment"""
+        # comment = Commment().edit_comment(comment_id, message)
+
     def delete_comment(self):
         pass
