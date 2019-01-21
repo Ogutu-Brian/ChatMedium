aa548bf043f63565ed557b9763cdc85c28c1cf82
bencyn <bensonnjung39@gmail.com>
2019-01-21 15:22:43 +0300

    [Start #163357136] add moderator class

---
+++ user/moderator.py
@@ -0,0 +1,11 @@
+from user.user import User, UserCollection
+class Moderator(User):
+    
+    def __init__(self):
+        super.__init__()
+        
+    def edit_comment(self):
+        pass
+    
+    def delete_comment(self):
+        pass
