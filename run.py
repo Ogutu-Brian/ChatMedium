ec8da0306a2619bc98fc2a364c27160c29edeacf
Ogutu Brian <codingbrian58@gmail.com>
2019-01-21 15:08:40 +0300

    [Feature #163359969] Add user sign up function

--- run.py
+++ run.py
@@ -6,14 +6,20 @@ def login_user(email="", password=""):
     """ add login data"""
     user = user_collection.query_by_field("email")
     response = None
-    if not user:
-        response = "A user with that email does not exist"
-    elif user.password == password:
-        response = user
+    if user:
+        if user.password == password:
+            response = user
     return response
 
-def logout_user()
-    
-    response = None
 
+def signup(first_name="", last_name="", password="", email=""):
+    """A function that creates a user account"""
+    response = None
+    if user_collection.query_by_field("email", email):
+        response = "A user with that email address already exists"
+    else:
+        user = User(first_name=first_name, last_name=last_name,
+                    password=password, email=email)
+        user_collection.add_user(user)
+        response = user
     return response
