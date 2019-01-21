11c37236b6e0e2806d09c1b25807392f6264ef42
Ogutu Brian <codingbrian58@gmail.com>
2019-01-21 12:56:16 +0300

    [Finishes #163357086] Add User collection class and query by field method

--- user/user.py
+++ user/user.py
@@ -14,3 +14,27 @@ class User:
         self.password = password
         self.email = email
         self.role = role
+        self.id = ""
+        self.full_name = self.first_name+" "+self.last_name
+
+
+class UserCollection:
+    """A class for storing user data"""
+
+    def __init__(self):
+        self.user_data = {}
+        self.id = 1
+
+    def add_user(self, user):
+        """Adds a user data"""
+        user.id = self.id
+        self.user_data[user.id] = user
+        self.id += 1
+
+    def query_by_id(self, item_id):
+        """Queries a user by the user id"""
+        response = None
+        for item in self.user_data.values():
+            if item.id == item_id:
+                response = item
+        return response
