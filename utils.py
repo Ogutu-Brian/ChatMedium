ebb14ffa3801b40b3a1d0571120646aa6cf0a14b
root <mugoh.ks@gmail.com>
2019-01-21 15:31:12 +0300

    [Starts #163361295] Return email on verification

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import re
 
 
 def verify_email(email):
-    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-        raise ValueError("Email format not invented\
+    while not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        # raise ValueError("Email format not invented\
+        #    Provide valid email")
+        print("Email format not invented\
             Provide valid email")
+        email = input("Enter email")
+
+    return email
