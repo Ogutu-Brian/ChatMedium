b89babd11f00d98c1d1d9b6916a74605aaa6af3f
root <mugoh.ks@gmail.com>
2019-01-21 13:04:56 +0300

    [Starts #163358476] Create UI display menu

---
+++ cli_script.py
@@ -0,0 +1,8 @@
+class Interface:
+
+    def __init__(self):
+        self.display_welcome_menu()
+
+    def display_welcome_menu(self):
+        print("\t\tWe call this - The Chat Medium")
+        print("\n\nChoose from the options below\n\n")
