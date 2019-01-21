4dd5881410f004ab0ba3fffd5e216cc653d3b019
khwilo <khwilowatai@gmail.com>
2019-01-21 12:54:38 +0300

    [Starts #163357032] Add a comments module

---
+++ comments/comment.py
@@ -0,0 +1,33 @@
+'''Module representing the comments'''
+from datetime import datetime
+
+COMMENTS = []
+
+class Comment:
+    '''Defintions for the comment entity'''
+    def __init__(self, author, message):
+        self.comment_id = len(COMMENTS) + 1
+        self.author = author
+        self.message = message
+        self.created_at = str(datetime.now())
+
+    def add_comment(self, comment):
+        '''Method to add a comment'''
+        COMMENTS.append(comment)
+
+    @staticmethod
+    def find_comment_by_author(author):
+        '''Find a comment by id'''
+        comment = {}
+        for index, _ in enumerate(COMMENTS):
+            if COMMENTS[index].get(author) == author:
+                comment = COMMENTS[index]
+        return comment
+
+    def comment_to_dict(self):
+        '''Return a dictionary of the comments object'''
+        return {
+            'author': self.author,
+            'message': self.message,
+            'created_at': self.created_at
+        }
