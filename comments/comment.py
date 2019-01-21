c00950241f3f0937aa80e4c2f0f08c8f399904b1
khwilo <khwilowatai@gmail.com>
2019-01-21 13:32:27 +0300

    [Finishes #163357032] Update the methods to save and find a comment

--- comments/comment.py
+++ comments/comment.py
@@ -1,27 +1,34 @@
 '''Module representing the comments'''
 from datetime import datetime
 
-COMMENTS = []
-
 class Comment:
     '''Defintions for the comment entity'''
+    COMMENTS = []
+
     def __init__(self, author, message):
-        self.comment_id = len(COMMENTS) + 1
+        self.comment_id = len(Comment.COMMENTS) + 1
         self.author = author
         self.message = message
         self.created_at = str(datetime.now())
 
-    def save(self, comment):
+    def save(self):
         '''Save a comment to the comments list'''
-        COMMENTS.append(comment)
+        comment = {
+            'comment_id': self.comment_id,
+            'author': self.author,
+            'message': self.message,
+            'created_at': self.created_at
+        }
+        Comment.COMMENTS.append(comment)
 
     @staticmethod
     def find_comment_by_author(author):
         '''Find a comment by author'''
+        comments_list = Comment.COMMENTS
         comment = {}
-        for index, _ in enumerate(COMMENTS):
-            if COMMENTS[index].get(author) == author:
-                comment = COMMENTS[index]
+        for index, _ in enumerate(comments_list):
+            if comments_list[index].get(author) == author:
+                comment = comments_list[index]
         return comment
 
     def comment_to_dict(self):
