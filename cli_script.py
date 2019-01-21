362a489be9fd4fd43c20c30ff8495bbce1130ed5
root <mugoh.ks@gmail.com>
2019-01-21 13:35:17 +0300

    [Feature #163358476] Add login option in display

--- cli_script.py
+++ cli_script.py
@@ -10,25 +10,34 @@ class Interface:
         print("\t\tWe call this - The Chat Medium")
         print("\n\nChoose from the options below\n\n")
 
-        self.give_access()
+        give_access()
 
-    @click.command()
-    @click.option('--access', type=click.choice([1, 2]),
-                  prompt="You are doing well. Choose a number from below\
-                  \n1. Register as new user\n2. Login")
-    def give_access(self, access):
-        print('You are doing well. Choose a number from below')
 
-        while access != 1 and access != 2:
-            click.echo("Illegal choice. Try 1 or 2")
-            access = input("\n1. Register as new user\n2. Login")
-        if access == 1:
-            self.register()
-        elif access == 2:
-            self.login_user()
+@click.command()
+@click.option('--access', type=click.Choice(['1', '2']),
+              prompt="You are doing well. Choose a number from below\
+              \n1. Register as new user\n2. Login")
+def give_access(access):
 
-    @click.command()
-    @click.option('--email', type=str, prompt=True)
-    @click.password_option('--password')
-    def register(self, value):
-        click.echo('User registeration success')
+    if access == '1':
+        register()
+    elif access == '2':
+        login_user()
+
+
+@click.command()
+@click.option('--email', type=str, prompt=True)
+@click.password_option('--password')
+def register(email, password):
+    click.echo('User registeration success')
+
+
+@click.command()
+@click.option('--email', type=str, prompt=True)
+@click.option('--password', prompt="Input password", hide_input=True)
+def login_user(email, password):
+    click.echo("Logging you in")
+
+
+if __name__ == '__main__':
+    s = Interface()
