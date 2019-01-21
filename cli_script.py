d7cf47c068b5bce4e7bb36a2f25b1307c6facf5a
root <mugoh.ks@gmail.com>
2019-01-21 13:22:41 +0300

    [Feature #163358476] Prompt user details for registration

--- cli_script.py
+++ cli_script.py
@@ -13,7 +13,7 @@ class Interface:
         self.give_access()
 
     @click.command()
-    @click.option('--access', type=int,
+    @click.option('--access', type=click.choice([1, 2]),
                   prompt="You are doing well. Choose a number from below\
                   \n1. Register as new user\n2. Login")
     def give_access(self, access):
@@ -22,3 +22,13 @@ class Interface:
         while access != 1 and access != 2:
             click.echo("Illegal choice. Try 1 or 2")
             access = input("\n1. Register as new user\n2. Login")
+        if access == 1:
+            self.register()
+        elif access == 2:
+            self.login_user()
+
+    @click.command()
+    @click.option('--email', type=str, prompt=True)
+    @click.password_option('--password')
+    def register(self, value):
+        click.echo('User registeration success')
